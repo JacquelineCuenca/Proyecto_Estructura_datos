7ac77bc66b176837edf0a0d96e067861dd36dd32
Vkrushenka <krushenkaurruti@gmail.com>
2025-08-17 09:51:17 -0500

    Update Clase Aeropuerto.java

--- app/src/main/java/com/example/myapplication/Clase Aeropuerto.java
+++ app/src/main/java/com/example/myapplication/Clase Aeropuerto.java
@@ -1,1 +1,20 @@
+package com.example.myapplication;
+
+public class Aeropuerto {
+    private String codigo;
+    private String nombre;
+
+    public Aeropuerto(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+
+    @Override
+    public String toString() {
+        return codigo + " - " + nombre;
+    }
+}
 
