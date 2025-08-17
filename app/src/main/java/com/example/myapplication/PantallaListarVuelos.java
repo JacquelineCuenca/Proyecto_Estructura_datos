a5e3e6013774b7909e52aaf14906f7658ed91799
Vkrushenka <krushenkaurruti@gmail.com>
2025-08-17 09:29:24 -0500

    Update PantallaListarVuelos.java

--- app/src/main/java/com/example/myapplication/PantallaListarVuelos.java
+++ app/src/main/java/com/example/myapplication/PantallaListarVuelos.java
@@ -1,1 +1,13 @@
+package com.example.myapplication;
+
+public class PantallaListarVuelos {
+    public void mostrarVuelos() {
+        System.out.println("\n--- Listado de Vuelos ---");
+
+        // Esto sería dinámico desde la lógica de negocio (Wentland)
+        System.out.println("DXG → JFK (13000 km)");
+        System.out.println("DXG → LHR (9200 km)");
+        System.out.println("JFK → LHR (5500 km)");
+    }
+}
 
