8cfb7a5e1a8a395099eb9d10b3c588a7439b522d
Vkrushenka <krushenkaurruti@gmail.com>
2025-08-19 10:23:52 -0500

    Update Vertex.java

--- app/src/main/java/Modelo/Vertex.java
+++ app/src/main/java/Modelo/Vertex.java
@@ -1,1 +1,22 @@
+package Modelo;
+
+import java.util.LinkedList;
+
+public class Vertex<V, E> {
+    private V content;
+    private LinkedList<Edge<E, V>> edges;
+
+    public Vertex(V content) {
+        this.content = content;
+        this.edges = new LinkedList<>();
+    }
+
+    public V getContent() {
+        return content;
+    }
+
+    public LinkedList<Edge<E, V>> getEdges() {
+        return edges;
+    }
+}
 
