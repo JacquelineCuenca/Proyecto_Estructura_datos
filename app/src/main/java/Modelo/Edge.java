4af093159f762e6f4444921fb973595c9f2c8cb2
Vkrushenka <krushenkaurruti@gmail.com>
2025-08-19 10:24:25 -0500

    Update Edge.java

--- app/src/main/java/Modelo/Edge.java
+++ app/src/main/java/Modelo/Edge.java
@@ -1,1 +1,32 @@
+package Modelo;
+
+public class Edge<E, V> {
+    private Vertex<V, E> source;
+    private Vertex<V, E> destination;
+    private int weight;
+    private E data;
+
+    public Edge(Vertex<V, E> source, Vertex<V, E> destination, int weight, E data) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+        this.data = data;
+    }
+
+    public Vertex<V, E> getSource() {
+        return source;
+    }
+
+    public Vertex<V, E> getDestination() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public E getData() {
+        return data;
+    }
+}
 
