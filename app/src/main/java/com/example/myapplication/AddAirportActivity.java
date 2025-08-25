741e900a5995bac37e081b40df18903d1f290937
SirProg <kevinmaldonadoparedes@gmail.com>
2025-08-24 22:50:26 -0500

    Modificaciones e interfaz

--- app/src/main/java/com/example/myapplication/AddAirportActivity.java
+++ app/src/main/java/com/example/myapplication/AddAirportActivity.java
@@ -1,10 +1,48 @@
 package com.example.myapplication;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import Modelo.Aeropuerto;
+import Modelo.DataRepository;
+
 public class AddAirportActivity extends AppCompatActivity {
+    private Button btnRegresarMenu, btnAnadir;
+    private TextView codigo, nombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_airport);
-}}
+
+        codigo = findViewById(R.id.editCodigo);
+        nombre = findViewById(R.id.editAeropuerto);
+
+        btnRegresarMenu = findViewById(R.id.btnRegresarAddAeropuerto);
+        btnRegresarMenu.setOnClickListener(view -> {
+            // Iniciar la actividad Pantalla_Menu
+            Intent intent = new Intent(AddAirportActivity.this, Pantalla_Menu.class);
+            startActivity(intent);
+        });
+
+        btnAnadir = findViewById(R.id.btnAnadirAeropuerto);
+
+        btnAnadir.setOnClickListener(view -> {
+            if(!codigo.getText().toString().isEmpty() && !nombre.getText().toString().isEmpty()){
+                addAirport(nombre.getText().toString(),codigo.getText().toString());
+                nombre.setText("");
+                codigo.setText("");
+                setResult(Activity.RESULT_OK);
+                finish();
+            }
+        });
+    }
+
+    private void addAirport(String nombre, String codigo) {
+        Aeropuerto aeropuerto = new Aeropuerto(codigo, nombre);
+        DataRepository.agregarAeropuerto(aeropuerto, AddAirportActivity.this);
+    }
+}
--- app/src/main/java/com/example/myapplication/RemoveAirportActivity.java
+++ app/src/main/java/com/example/myapplication/RemoveAirportActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RemoveAirportActivity extends AppCompatActivity{
+    Button btnRegresarMenu, btnEliminar, btnInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
